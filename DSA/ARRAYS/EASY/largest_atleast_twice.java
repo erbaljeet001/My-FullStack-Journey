646279d6871d170c731fc631b771518636bbd554
Baljeet Kumar Yadav <baljeetydv0001@gmail.com>
2026-07-07 18:49:57 +0530

    add solution of find  largest element at least twice of other

---
+++ DSA/ARRAYS/EASY/largest_atleast_twice.java
@@ -0,0 +1,40 @@
+public class largest_atleast_twice {
+  
+    public int dominantIndex(int[] nums) {
+        
+        int n= nums.length;
+        int largest = Integer.MIN_VALUE;
+        int Sec_largest = Integer.MIN_VALUE;
+        int largest_ind = -1; 
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]> largest)
+            {
+                largest = nums[i];
+                largest_ind= i;
+
+            }
+        }
+
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i] > Sec_largest && nums[i]!= largest)
+            {
+                Sec_largest= nums[i];
+                
+            }
+        }
+
+        if(largest >= 2*Sec_largest)
+        {
+            return largest_ind;
+        }
+        else
+        {
+              return -1;
+        }
+        
+
+    }
+}
+
