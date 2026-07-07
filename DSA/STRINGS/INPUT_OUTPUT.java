b6067b098aadfb4e596cafaae63bbc606780d6aa
Baljeet Kumar Yadav <baljeetydv0001@gmail.com>
2026-07-07 21:08:12 +0530

    String Basics

---
+++ DSA/STRINGS/INPUT_OUTPUT.java
@@ -0,0 +1,23 @@
+package DSA.STRINGS;
+import java.util.*;
+
+public class INPUT_OUTPUT 
+{
+   public static void main(String[] args) {
+     {
+       Scanner sc = new Scanner(System.in);
+       
+       System.out.println("Enter the name :");
+
+       String T = sc.nextLine();
+       System.out.println("name :" + T);
+       
+       System.out.println("Enter the name :");
+       String x = sc.next();
+       System.out.println("name :" + x);
+       
+       
+
+    }
+   }
+}
