594d70ef9ab6205894d68b913d33477c0e4b5289
Baljeet Kumar Yadav <baljeetydv0001@gmail.com>
2026-07-07 18:17:01 +0530

    add solution of find Second Largest element in array

---
+++ DSA/ARRAYS/EASY/Solution.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public int getSecondLargest(int[] arr) {
+        int n= arr.length;
+        int largest = Integer.MIN_VALUE;
+        int Sec_largest = Integer.MIN_VALUE;
+        for(int i=0;i<n;i++)
+        {
+             if(arr[i] > largest )
+             {
+                  largest= arr[i];
+             }
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+             if(arr[i] > Sec_largest && arr[i]!= largest)
+             {
+                 Sec_largest= arr[i];
+             }
+        }
+   if(Sec_largest == Integer.MIN_VALUE)
+     return -1;
+     
+     return Sec_largest;
+     
+        
+    }
+    
+} 
+
